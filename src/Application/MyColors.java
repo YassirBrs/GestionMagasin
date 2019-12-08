2856cbf26f788995a74184ae118c42bc9787f98c
YassirBrs <yassirbouras10@gmail.com>
2019-12-08 19:25:12 +0100

    Version 1: +Products Management +Categories Management +Clients Management +Interface JavaFX

---
+++ src/Application/MyColors.java
@@ -0,0 +1,8 @@
+package Application;
+
+public class MyColors {
+	public static String c1 = "#1abc9c";
+	public static String c2 = "#ecf0f1";
+	public static String c3 = "#bdc3c7";
+	public static String c4 = "#1abc9c";
+}
---
+++ src/CategorieField/CategorieDao.java
@@ -0,0 +1,13 @@
+package CategorieField;
+
+import java.util.Collection;
+
+public interface CategorieDao {
+
+	public void insert(Categorie c);
+	public void update(Categorie c);
+	public void delete(long codecateg);
+	public Collection<Categorie> getAll();
+	public Categorie getOne(long id);
+	
+}
